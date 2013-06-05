5f513f18dc01f93beb0c3603b78313b998473839
Nick Jennings <nick@silverbucket.net>
2013-06-06 00:48:46 +0200

    added initial functionality to add new feed urls (not yet working) and display status messages (error, success)

--- res/js/dogfeed.js
+++ res/js/dogfeed.js
@@ -1,6 +1,4 @@
-angular.module('dogfeed', []).
-
-
+angular.module('dogfeed', ['ngRemoteStorage']).
 
 
 /**
@@ -43,7 +41,6 @@ function ($rootScope) {
   });
 
   $rootScope.$on('closeModalAddFeed', function(event, args) {
-    //console.log('closeModalSockethubSettings');
     $("#modalAddFeed").modal('hide');
   });
 }]).
@@ -88,15 +85,31 @@ function ($scope, $rootScope) {
  * controller: addFeedCtrl
  */
 controller('addFeedCtrl',
-['$scope',
-function ($scope) {
+['$scope', 'RS', '$rootScope',
+function ($scope, RS, $rootScope) {
   $scope.url = '';
+  $scope.adding = false;
 
   $scope.add = function (url) {
-    console.log('add url! ' + url);
+    $scope.adding = true;
+
+    var obj = {
+      url: url,
+      name: url,
+      cache_articles: 20,
+      last_fetched: new Date().getTime()
+    };
+
+    RS.call('rss', 'add', [obj]).then(function () {
+      console.log('rss feed url saved!');
+      $rootScope.$broadcast('closeModalAddFeed');
+      $scope.adding = false;
+      $rootScope.$broadcast('message', {type: 'success', message: 'RSS feed added: '+url});
+    }, function (err) {
+      $rootScope.$broadcast('message', {type: 'error', message: err.message});
+    });
   };
 
-  $scope.adding = false;
 }]).
 
 
@@ -120,6 +133,101 @@ function ($scope) {
 //
 ///////////////////////////////////////////////////////////////////////////
 
+
+
+/**
+ * directive: message
+ */
+directive('message',
+['$rootScope', '$timeout',
+function ($rootScope, $timeout) {
+  return {
+    restrict: 'A',
+    template: '<div class="alert alert-{{ m.type }}" ng-show="haveMessage">'+
+              '  <strong>{{ m.title }}</strong> ' +
+              '  <span> {{ m.message }}</span>' +
+              '</div>',
+    link: function (scope) {
+      scope.haveMessage = false;
+      scope.m = {type: '', title: '', message: ''};
+
+
+      var presets = {
+        'remotestorage-connect': {
+          type: 'error',
+          title : 'Connect to remoteStorage',
+          message: 'First things first. You must connect to your remoteStorage'
+        },
+        'sockethub-config': {
+          type: 'error',
+          title: 'Sockethub configuration needed',
+          message: 'You must fill in your Sockethub connection details'
+        },
+        'sockethub-connect': {
+          type: 'error',
+          title: 'Sockethub connection error',
+          message: 'Unable to connect to Sockethub please check your configuration and try again'
+        },
+        'sockethub-register': {
+          type: 'error',
+          title: 'Sockethub registration problem',
+          message: 'We were unable to register with your Sockethub instance'
+        },
+        'xmpp-connect': {
+          type: 'error',
+          title: 'XMPP connection failed',
+          message: 'There was a problem connecting to the XMPP server, please verify you settings'
+        },
+        'unknown': {
+          type: 'error',
+          title: 'An unknown error has occurred',
+          message: ''
+        }
+      };
+
+      $rootScope.$on('message', function (event, e) {
+        console.log('message event: ', e);
+
+        scope.haveMessage = false;
+
+        if (typeof e === 'undefined') {
+          e = 'no error specified';
+        }
+
+        /*if ((typeof e.type === 'undefined') ||
+            (typeof presets[e.message] === 'undefined')) {
+          scope.m = presets['unknown'];
+          scope.m.message = String(e.message || e);
+        } else {*/
+
+        if (typeof presets[e.message] !== 'undefined') {
+          scope.m = presets[e.message];
+        } else if (typeof e.message === 'string') {
+          if (e.type === 'success') {
+            scope.m.title = 'Success';
+          } else {
+            scope.m.title = "Error";
+          }
+          scope.m.message = e.message;
+          scope.m.type = e.type;
+        }
+        console.log('done processing: ', scope.m);
+
+        scope.haveMessage = true;
+        $timeout(function () {
+          scope.haveMessage = false;
+          scope.m = {type: '', title: '', message: ''};
+        }, 4000);
+
+
+        /*if (e.type === 'sockethub-config') {
+          console.log('no config found, launch modal');
+          $rootScope.$broadcast('showModalSettingsSockethub', {locked: true});
+        }*/
+      });
+    }
+  };
+}]).
 
 /**
  * directive: articles
@@ -163,6 +271,7 @@ function () {
       feeds: '='
     },
     template: '<h4 ng-transclude></h4>' +
+              ' <span>{{ message }}<span>' +
               '<ul class="nav nav-list">' +
               '  <li ng-repeat="f in uniqueFeeds" data-toggle="tooltip" title="{{ f.address }}">' +
               '    <a href="#/{{f.platform}}/feed/{{ f.address | urlEncode }}">{{ f.name }}</a>' +
@@ -170,6 +279,7 @@ function () {
               '</ul>',
     link: function (scope, element, attrs) {
       scope.uniqueFeeds = [];
+      scope.message = '';
 
       for (var i = 0, num = scope.feeds.length; i < num; i = i + 1) {
         var match = false;
@@ -188,6 +298,10 @@ function () {
       }
       console.log('**** feeds: ', scope.feeds);
       console.log('**** uniqueFeeds: ', scope.uniqueFeeds);
+
+      if (scope.uniqueFeeds.length === 0) {
+        scope.message = "no feeds yet, add some!";
+      }
     },
     transclude: true
   };
@@ -198,7 +312,6 @@ function () {
 /*
               <div ng-controller="feedListCtrl">
                 <h4>Feeds</h4>
-
                 <ul class="nav nav-list">
                   <li data-ng-repeat="c in model.contacts | filter:c.name | orderBy:c.state" ng-class="feedSwitch('{{ c.address }}')">
                     <a href="#/feed/{{c.address}}">
@@ -209,18 +322,15 @@ function () {
                 </ul>
               </div>
 */
-
 /*
               <div class="span9 messages" ng-controller="articlesCtrl">
                 <p>{{ model.currentName }} - {{ model.currentAddress }}</p>
                 <div class="articles">
-
                   <div class="article well" ng-repeat="a in model.articles">
                     <div class="name"><small>{{ a.actor.name }}</small></div>
                     <div class="address"><small>{{ a.actor.address }}</small></div>
                     <div class="text"><p></p><p class="text-info">{{ a.object.brief_text }}<p></div>
                   </div>
-
                 </div>
               </div>
 */
