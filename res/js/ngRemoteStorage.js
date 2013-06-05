20107fb137c0d41a509ea2b6c9d5f20054c96d9a
Nick Jennings <nick@silverbucket.net>
2013-06-06 00:45:20 +0200

    remoteStorage angular factory

---
+++ res/js/ngRemoteStorage.js
@@ -0,0 +1,57 @@
+var ngRemoteStorage = angular.module('ngRemoteStorage', []).
+factory('RS', ['$rootScope', '$q',
+function ($rootScope, $q) {
+
+  return {
+    isConnected: function () {
+      if (remoteStorage.getBearerToken() === null) {
+        return false;
+      } else {
+        return true;
+      }
+    },
+    call: function (module, func, params) {
+      var defer = $q.defer();
+      console.log('RS.call('+module+', '+func+', params):', params);
+
+      if ((typeof params === 'object') &&
+          (typeof params[0] === 'undefined')) {
+        defer.reject('RS.call params must be an array');
+      } else {
+        try {
+          remoteStorage[module][func].apply(null, params).
+            then(function (res) {
+              $rootScope.$apply(function () {
+                defer.resolve(res);
+              });
+            }, function (err) {
+              $rootScope.$apply(function () {
+                defer.reject(err);
+              });
+            });
+        } catch (e) {
+          console.log('error : ',e);
+          defer.reject(e);
+        }
+      }
+      return defer.promise;
+    }
+  };
+}]);
+
+
+ngRemoteStorage.controller('remoteStorageCtrl', ['$scope', '$route', '$routeParams', '$location', 'RS',
+function ($scope, $route, $routeParams, $location, RS) {
+
+  remoteStorage.util.silenceAllLoggers();
+
+  remoteStorage.claimAccess({sockethub:'rw',rss:'rw',news:'rw'}).then(function () {
+    remoteStorage.displayWidget('remotestorage-connect', {
+      redirectUri: window.location.protocol + '//' + window.location.host + '/rscallback.html'
+    });
+
+    remoteStorage.sockethub.init();
+    remoteStorage.rss.init();
+  });
+
+}]);
