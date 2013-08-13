ec01c4e07b198e5aea822a99a7f1038a900eb82b
Nick Jennings <nick@silverbucket.net>
2013-08-13 23:55:35 +0200

    don't display read messages by default, allow toggle, and display message when no new messages are there for reading. fixes #27 fixes #21

--- res/js/ngRSS.js
+++ res/js/ngRSS.js
@@ -122,19 +122,19 @@ function ($q, SH, CH, RS, RSutil, $rootScope) {
 
   // add a new feed to remoteStorage
   function _add(obj) {
-    console.log('ADDING FEED TO DATA STRUCT: ', obj);
+    //console.log('ADDING FEED TO DATA STRUCT: ', obj);
     data.info[obj.url] = obj;
     var match = false;
     for (var i = 0, len = data.infoArray.length; i < len; i = i + 1) {
       if ((data.infoArray[i]) && (data.infoArray[i].url === obj.url)) {
-        console.log('BREAK: '+obj.url);
+        //console.log('BREAK: '+obj.url);
         match = true;
         break;
       }
     }
     if (!match) {
-      console.log('adding to list: ', data.info[obj.url]);
-      console.log('existing feeds: ', data.info);
+      ///console.log('adding to list: ', data.info[obj.url]);
+      //console.log('existing feeds: ', data.info);
       data.infoArray.push(data.info[obj.url]);
       data.info[obj.url]['loaded'] = false;
       func.fetchFeed(obj.url).then(function () {
@@ -151,7 +151,7 @@ function ($q, SH, CH, RS, RSutil, $rootScope) {
       _add(obj);
     } else {
       RS.call('rss', 'add', [obj]).then(function (m) {
-        console.log('feed added to rs: ', obj);
+        //console.log('feed added to rs: ', obj);
         _add(obj);
         defer.resolve(m);
       }, function (err) {
@@ -164,7 +164,7 @@ function ($q, SH, CH, RS, RSutil, $rootScope) {
   func.updateFeed = function updateFeed(obj) {
     var defer = $q.defer();
     RS.call('rss', 'add', [obj]).then(function (m) {
-      console.log('feed updated: ', obj);
+      //console.log('feed updated: ', obj);
       data.info[obj.url] = obj;
       defer.resolve(m);
     }, function (err) {
@@ -176,11 +176,11 @@ function ($q, SH, CH, RS, RSutil, $rootScope) {
   func.removeFeed = function removeFeed(url) {
     var defer = $q.defer();
     RS.call('rss', 'remove', [url]).then(function (m) {
-      console.log('feed removed: ', url);
+      //console.log('feed removed: ', url);
       delete data.info[url];
       for (var i = 0, len = data.infoArray.length; i < len; i = i + 1) {
         if ((data.infoArray[i]) && (data.infoArray[i].url === url)) {
-          console.log('removing from list: ',data.infoArray[i]);
+          //console.log('removing from list: ',data.infoArray[i]);
           data.infoArray.splice(i, 1);
           break;
         }
@@ -279,12 +279,12 @@ function ($q, SH, CH, RS, RSutil, $rootScope) {
     }
 
     if (m.status) {
-      console.log('adding article: ', m);
+      //console.log('adding article: ', m);
       var id = RSutil.encode(m.object.link);
-      console.log('ID: ', id);
+      //console.log('ID: ', id);
       RS.call('articles', 'get', [id]).then(function (a) {
         if (a) {
-          console.log('ARTICLE FETCH from RS: ', a);
+          //console.log('ARTICLE FETCH from RS: ', a);
           m.object.read = (a.read) ? a.read : false;
         }
 
@@ -338,7 +338,7 @@ function ($scope, RSS, $rootScope) {
     };
 
     RSS.func.addFeed(obj).then(function (m) {
-      console.log('rss feed url saved!: ', m);
+      //console.log('rss feed url saved!: ', m);
       $rootScope.$broadcast('closeModalAddFeed');
       $scope.adding = false;
       $rootScope.$broadcast('message', {type: 'success', message: 'RSS feed added: '+obj.url});
@@ -357,8 +357,10 @@ function ($scope, RSS, $rootScope) {
 controller('feedCtrl',
 ['$scope', 'RSS', 'util', '$rootScope', '$timeout',
 function ($scope, RSS, util, $rootScope, $timeout) {
-
   $scope.model = {};
+  $scope.model.settings = {
+    showRead: false
+  };
   $scope.model.feeds = RSS.data;
   $scope.model.loading = true;
   $scope.model.feeds.current = {
@@ -373,7 +375,7 @@ function ($scope, RSS, util, $rootScope, $timeout) {
   }
   $scope.model.saving = false;
 
-  $scope.isSelected = function(url, inclusive) {
+  $scope.isSelected = function (url, inclusive) {
     if ($scope.model.feeds.current.indexes.length === 0) {
       if ((inclusive) || (!url)) {
         return true;
@@ -390,15 +392,38 @@ function ($scope, RSS, util, $rootScope, $timeout) {
     return false;
   };
 
+  $scope.isShowable = function (feedUrl, isRead, settings) {
+    if (!$scope.isSelected(feedUrl, true)) {
+      return false;
+    } else if (isRead) {
+      if (settings.showRead) {
+        return true;
+      } else {
+        return false;
+      }
+    }
+    return true;
+  };
+
+  // returns true if current selection is empty (has no unread articles)
+  $scope.currentIsEmpty = function () {
+    if (!$scope.model.feeds.current.name) {
+      return false;
+    }
+    for (var i = 0, num = $scope.model.feeds.current.indexes.length; i < num; i = i + 1) {
+      if ($scope.model.feeds.info[$scope.model.feeds.current.indexes[i]].unread > 0) {
+        return false;
+      }
+    }
+    return true;
+  };
 
   $scope.switchFeed = function (url) {
     if (!url) {
       $scope.model.feeds.current.name = '';
-      $scope.model.feeds.current.url = '';
       $scope.model.feeds.current.indexes.length = 0;
     } else {
       $scope.model.feeds.current.name = RSS.data.info[url].name;
-      $scope.model.feeds.current.url = url;
       $scope.model.feeds.current.indexes = [url];
     }
   };
@@ -409,7 +434,7 @@ function ($scope, RSS, util, $rootScope, $timeout) {
     //$scope.switchFeed(url);
     $scope.model.feeds.edit = url;
     $scope.model.feeds._editName = $scope.model.feeds.info[$scope.model.feeds.edit].name;
-    console.log('EDIT: ', $scope.model.feeds.edit);
+    //console.log('EDIT: ', $scope.model.feeds.edit);
     $("#modalFeedSettings").modal({
       show: true,
       keyboard: true,
@@ -426,7 +451,7 @@ function ($scope, RSS, util, $rootScope, $timeout) {
   $scope.saveFeedSettings = function (url) {
     $scope.saving = true;
     //$scope.model.feeds.info[$scope.model.feeds.edit].name = $scope.model.feeds.edit.name;
-    console.log('SAVE: ', $scope.model.feeds.info[url]);
+    //console.log('SAVE: ', $scope.model.feeds.info[url]);
     RSS.func.updateFeed($scope.model.feeds.info[url]).then(function () {
       $("#modalFeedSettings").modal('hide');
       $rootScope.$broadcast('message', {type: 'success', message: 'updated feed '+url});
@@ -454,7 +479,7 @@ function ($scope, RSS, util, $rootScope, $timeout) {
   };
 
   $scope.markRead = function (url) {
-    console.log('markRead Called!');
+    //console.log('markRead Called!');
     for (var i = 0, num = $scope.model.feeds.articles.length; i < num; i = i + 1) {
       //console.log('A.link: ' + $scope.model.feeds.articles[i].object.link + ' url: '+url);
       if ($scope.model.feeds.articles[i].object.link === url) {
@@ -497,14 +522,25 @@ function ($scope, RSS, util, $rootScope, $timeout) {
  */
 directive('feedList', [
 function () {
-
-
   return {
     restrict: 'E',
     scope: {
-      'feeds': '='
+      'feeds': '=',
+      'settings': '='
     },
-    template: '<h4 ng-transclude></h4>' +
+    template: /*
+              '<div style="min-width: 170px; background-color: #eeeeee;" ng-controller="feedCtrl">' +
+              '  <form name="quickSettings" class="form-horizontal" novalidate>' +
+              '    <div class="control-group">' +
+              '      <label for="showRead" style="width: 130px; display: font-size: 12px; margin-right: 20px;" class="control-label">Show Read Artciles</label>' +
+              '      <div class="controls">' +
+              '        <input type="checkbox" name="showRead" ng-model="model.settings.showRead" />' +
+              '      </div>' +
+              '    </div>' +
+              '  </form>' +
+              '</div>' +
+              */
+              '<h4 ng-transclude></h4>' +
               '<ul class="nav nav-list" ng-controller="feedCtrl">' +
               '  <li ng-click="switchFeed()"' +
               '       ng-class="{active: isSelected(), \'all-feeds\': true}">' +
@@ -527,13 +563,7 @@ function () {
               '    </div>' +
               '  </li>' +
               '</ul>',
-    transclude: true,
-    link: function (scope, element, attrs) {
-      console.log('attrs:', attrs);
-      scope.$watch(attrs.feeds.info, function (val) {
-        console.log('attrs.feeds.info val:', val);
-      });
-    }
+    transclude: true
   };
 }]).
 
@@ -546,13 +576,16 @@ function () {
   return {
     restrict: 'E',
     scope: {
-      'feeds': '='
+      'feeds': '=',
+      'settings': '='
     },
-    template: '<h4>{{ feeds.current.name }}</h4>' +
+    template: //'<h4><span ng-bind="feeds.current.name"></span></h4>' +
+              '<div class="article-text" ng-controller="feedCtrl" ng-show="feeds.articles.length > 0 && currentIsEmpty()"><p>no new articles</p></div>' +
               '<div ng-repeat="a in (filteredItems = (feeds.articles | orderBy: \'object.date\':true))"' +
               '     ng-controller="feedCtrl"' +
               '     ng-click="markRead(a.object.link)"' +
-              '     ng-class="{well: true, hide: !isSelected(a.actor.address, true), unread: !a.object.read, article: true}" >' +
+              '     ng-class="{well: true, unread: !a.object.read, article: true}"' +
+              '     ng-show="isShowable(a.actor.address, a.object.read, settings)">' +
               '  <h2>{{ a.object.title }}</h2>' +
               '  <p>feed: <i>{{ a.actor.name }}</i></p>' +
               '  <p>date: <i>{{ a.object.date }}</i></p>' +
@@ -564,9 +597,9 @@ function () {
       //console.log('#### LINK FUNCTION: element: ', element);
       //console.log('#### LINK FUNCTION: attrs: ', attrs);
 
-      scope.$watch(attrs.feeds, function (val) {
+      /*scope.$watch(attrs.feeds, function (val) {
         console.log('WATCH attrs.feeds', val);
-      });
+      });*/
 
       var divs = document.getElementsByClassName('article');
       for (var i = 0, num = divs.length; i < num; i = i + 1) {
